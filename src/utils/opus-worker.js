4ebaa9e31b97849614fdceb7e110a531a04f1c97
Paulo Vieira <paulo.vieira@kelvininc.com>
2018-11-16 20:54:20 +0000

    adding the option to the initial options to activate or not the native support and also the sample rate

--- src/utils/opus-worker.js
+++ src/utils/opus-worker.js
@@ -1,36 +1,37 @@
 import Event from './event.js';
 export default class OpusWorker extends Event {
-    constructor(channels, libopusPath) {
-        super('worker');
-        this.worker = new Worker(libopusPath);
-        this.worker.addEventListener('message', this.onMessage.bind(this));
-        this.worker.postMessage({
-            type: 'init',
-            config: {
-                rate:24000,
-                channels:channels
-            }
-        });
-    }
+	constructor(channels, libopusPath, sampleRate) {
+		super('worker');
+		this.worker = new Worker(libopusPath);
+		this.sampleRate = sampleRate;
+		this.worker.addEventListener('message', this.onMessage.bind(this));
+		this.worker.postMessage({
+			type: 'init',
+			config: {
+				rate: sampleRate,
+				channels: channels
+			}
+		});
+	}
 
-    getSampleRate() {
-        return 24000;
-    }
+	getSampleRate() {
+		return this.sampleRate;
+	}
 
-    decode(packet) {
-        let workerData = {
-            type: 'decode',
-            buffer: packet
-        };
-        this.worker.postMessage(workerData);
-    }
+	decode(packet) {
+		let workerData = {
+			type: 'decode',
+			buffer: packet
+		};
+		this.worker.postMessage(workerData);
+	}
 
-    onMessage(event) {
-        let data = event.data;
-        this.dispatch('data', data.buffer);
-    }
-    destroy() {
-        this.worker = null;
-        this.offAll();
-    }
+	onMessage(event) {
+		let data = event.data;
+		this.dispatch('data', data.buffer);
+	}
+	destroy() {
+		this.worker = null;
+		this.offAll();
+	}
 }
